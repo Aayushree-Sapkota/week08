7842398461888ac24763f63fcf802d7ef87ca9a4
Aayushree Sapkota <s225598173@deakin.edu.au>
2026-09-19 13:59:01 +1000

    Configure Continuous Deployment in workflows and add Terraform configuration

---
+++ terraform/versions.tf
@@ -0,0 +1,27 @@
+terraform {
+    required_version = ">= 1.7.0"
+
+    required_providers {
+        azurerm = {
+            source  = "hashicorp/azurerm"
+            version = "~> 4.0"
+        }
+    }
+
+    # REMOTE STATE BACKEND
+    backend "azurerm" {
+        resource_group_name = "tfstate-rg"
+        storage_account_name = "tfstate20245" 
+        container_name = "tfstate"
+        key = "koalatech.tfstate"
+  }
+
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
